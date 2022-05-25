d8ef7cb7d0c693c102d3f784d4fed7376605a9fa
LargeSloth <skijumper2010@gmail.com>
2022-05-25 02:40:14 -0500

    Change hours for hourDisplay fix typo in dayDisplay

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 function hour() {
     const hourDisplay = new Date().getHours();
     if (hourDisplay < 5) return "Go To Sleep"
-    if (hourDisplay < 8) return "Good Morning"
-    if (hourDisplay < 13) return "Good Afternoon"
-    if (hourDisplay < 18) return "Good Evening"
+    if (hourDisplay < 11) return "Good Morning"
+    if (hourDisplay < 15) return "Good Afternoon"
+    if (hourDisplay < 24) return "Good Evening"
 }
 document.getElementById("hourDisplay").innerHTML = hour();
 
@@ -14,7 +14,7 @@ function day() {
     const dayDisplay = new Date().getDay();
     if (dayDisplay === 0) return "It's The Lords Day"
     if (dayDisplay === 1) return "It's Grody Monday"
-    if (dayDisplay === 2) return "It's Tusday" 
+    if (dayDisplay === 2) return "It's Tuesday" 
     if (dayDisplay === 3) return "It's Hump Day"
     if (dayDisplay === 4) return "It's Thursday"
     if (dayDisplay === 5) return "It's Friday Friday Gotta Get Down On Friday"
